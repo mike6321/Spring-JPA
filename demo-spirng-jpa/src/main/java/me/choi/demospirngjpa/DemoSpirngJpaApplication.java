e300b86bea5352310e18c013806e91eed366f5ce
junwoo <33277588+mike6321@users.noreply.github.com>
2021-02-19 11:13:55 +0900

    Spring-JPA Project init

---
+++ demo-spirng-jpa/src/main/java/me/choi/demospirngjpa/DemoSpirngJpaApplication.java
@@ -0,0 +1,13 @@
+package me.choi.demospirngjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpirngJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpirngJpaApplication.class, args);
+    }
+
+}
---
+++ demo-spirng-jpa/src/test/java/me/choi/demospirngjpa/DemoSpirngJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.choi.demospirngjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpirngJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
