5eef940260c278fe3d3487d11505e7101e9b6a20
junwoo <33277588+mike6321@users.noreply.github.com>
2021-05-17 21:49:37 +0900

    (jpashop) View 환경 설정

---
+++ jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Project : jpashop
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 9:40 오후
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!!");
+        return "hello";
+    }
+}
