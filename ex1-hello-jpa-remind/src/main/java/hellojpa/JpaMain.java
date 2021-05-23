329c8fb6b04c05a70606240b1dd4d7234fbfc4ee
junwoo <33277588+mike6321@users.noreply.github.com>
2021-05-23 23:34:01 +0900

    (ex1-hello-jpa-remind) 플러시

--- ex1-hello-jpa-remind/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa-remind/src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 /**
  * Project : ex1-hello-jpa-remind
@@ -23,39 +22,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member member = new Member();
-//            member.setId(102L);
-//            member.setName("HelloJPA");
-//
-//            em.persist(member);
-
-            // TODO: 1차 캐시 2021/05/23 9:27 오후
+            // TODO: 플러시 2021/05/23 11:27 오후
             /**
-             * - persist를 하고 동일한 값을 찾으면 1차 캐시에서 바로 찾기 때문에 select 쿼리가 생성되지 않는다.
-             * - persist를 하고 다른 값을 찾으면 DB에서 값을 찾아야하기 때문에 select 쿼리가 생성된다.
-             * - persist를 하지 않고 값을 찾으면 첫 번째 탐색 시에는 DB에서 값을 찾지만 두 번째 탐색 시 부턴 1차캐시에서 값을 탐색하므로
-             *   select 쿼리가 발생하지 않는다.
-             *   (새로운 EntityManager이기 때문이다.)
-             * - find는 커밋 시점이 아니라 호출 시점에 select 쿼리가 나간다.
+             * commit 시점에 쿼리가 나가는 것이 아닌 flush 하는 시점에 쿼리가 나간다.
+             * jpql을 사용하는 시점에 flush가 된다.
              * */
-            System.out.println("***********BEFORE***********");
-            Member findMember1 = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L);
-            System.out.println("***********AFTER***********");
+            Member member = new Member(200L, "member200");
+            em.persist(member);
 
-            System.out.println("findMember.getId() = " + findMember1.getId());
-            System.out.println("findMember.getName() = " + findMember1.getName());
-
-            System.out.println("findMember1 == findMember2 :: " + (findMember1 == findMember2));
-            
-            
-            // TODO: 변경 감지 2021/05/23 9:55 오후 
-            /**
-             * set만 했을 뿐인데 데이터가 변경되었다.
-             * (persist는 할 필요가 없다. )
-             * - 1차
-             * */
-            findMember2.setName("jwdeveloper");
+            em.flush();
+            System.out.println("***************************");
 
             tx.commit();
         } catch (Exception e) {
