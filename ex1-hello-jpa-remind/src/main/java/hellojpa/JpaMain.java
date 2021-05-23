28edd393f90cd34be8d91b76fcfdc3c5b5aebd21
junwoo <33277588+mike6321@users.noreply.github.com>
2021-05-23 19:33:26 +0900

    (ex1-hello-jpa-remind) 영속성 컨텍스트1

--- ex1-hello-jpa-remind/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa-remind/src/main/java/hellojpa/JpaMain.java
@@ -23,27 +23,19 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // TODO: 삽입 2021/05/23 5:13 오후
-//            Member member = new Member();
-//            member.setId(1L);
-//            member.setName("HelloA");
+            // TODO: 비영속 상태 2021/05/23 7:29 오후
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
 
-//            em.persist(member);
+            // TODO: 영속 상태 2021/05/23 7:30 오후
+            /**
+             * DB에 저장되는 상태는 아니다.
+             * */
+            System.out.println("=========BEFORE=========");
+            em.persist(member);
+            System.out.println("=========AFTRE=========");
 
-            // TODO: 조회1 2021/05/23 5:13 오후
-//            Member findMember = em.find(Member.class, 1L);
-//            System.out.println("findMember = " + findMember.getId());
-//            System.out.println("findMember = " + findMember.getName());
-
-            // TODO: 조회2 2021/05/23 5:26 오후
-            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
-            for (Member member : result) {
-                System.out.println("member.id = " + member.getId());
-                System.out.println("member.name = " + member.getName());
-            }
-
-            // TODO: 수정 2021/05/23 5:14 오후
-//            findMember.setName("HelloJPA");
 
             tx.commit();
         } catch (Exception e) {
