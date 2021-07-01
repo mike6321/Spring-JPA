894104a226fc74469c85de11cf627da82dd37516
junwoo <33277588+mike6321@users.noreply.github.com>
2021-07-02 00:25:05 +0900

    (ex1-hello-jpa-remind) 다대일 [N:1](2) - 다대일 양방향

--- ex1-hello-jpa-remind/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa-remind/src/main/java/hellojpa/Member.java
@@ -47,5 +47,4 @@ public class Member {
         this.team = team;
     }
 
-
 }
--- ex1-hello-jpa-remind/src/main/java/hellojpa/Team.java
+++ ex1-hello-jpa-remind/src/main/java/hellojpa/Team.java
@@ -1,6 +1,8 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Project : ex1-hello-jpa-remind
@@ -19,6 +21,9 @@ public class Team {
     @Column(name = "TEAMNAME")
     private String name;
 
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
     public Long getId() {
         return id;
     }
@@ -34,4 +39,5 @@ public class Team {
     public void setName(String name) {
         this.name = name;
     }
+
 }
