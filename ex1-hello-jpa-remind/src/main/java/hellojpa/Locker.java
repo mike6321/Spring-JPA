87d12a1a6cebc8b39de4914b12a5db05d61cc36c
junwoo <33277588+mike6321@users.noreply.github.com>
2021-07-13 02:05:24 +0900

    (ex1-hello-jpa-remind) 일대일[1:1](1) - 주 테이블에 외래 키 단방향

---
+++ ex1-hello-jpa-remind/src/main/java/hellojpa/Locker.java
@@ -0,0 +1,24 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+/**
+ * Project : ex1-hello-jpa-remind
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 1:01 오전
+ */
+@Entity
+public class Locker {
+
+    @Id @GeneratedValue
+    @Column(name = "LOCKER_ID")
+    private Long id;
+
+    private String name;
+
+    @OneToOne(mappedBy = "locker")
+    private Member member;
+
+}
