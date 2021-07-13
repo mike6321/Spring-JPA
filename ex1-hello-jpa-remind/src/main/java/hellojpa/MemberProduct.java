7071cee4059d0e15f6aa9ded0caa37e3209849b3
junwoo <33277588+mike6321@users.noreply.github.com>
2021-07-13 17:40:52 +0900

    (ex1-hello-jpa-remind) 다대다[N:N](2) - 다대다 한계 극복

---
+++ ex1-hello-jpa-remind/src/main/java/hellojpa/MemberProduct.java
@@ -0,0 +1,31 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * Project : ex1-hello-jpa-remind
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 4:56 오후
+ */
+@Entity
+public class MemberProduct {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+
+    private int count;
+    private int price;
+    private LocalDateTime orderDateTime;
+
+}
