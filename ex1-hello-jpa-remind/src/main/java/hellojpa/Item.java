fe722da2d9147c0537dfad77f24e922a0069ab21
최준우 <mike6321@nhnsoft.com>
2021-07-20 16:29:58 +0900

    (ex1-hello-jpa-remind) 상속관계 매핑(3) - 단일 테이블 전략

--- ex1-hello-jpa-remind/src/main/java/hellojpa/Item.java
+++ ex1-hello-jpa-remind/src/main/java/hellojpa/Item.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
  * Time : 12:24 오전
  */
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn
 public class Item {
 
